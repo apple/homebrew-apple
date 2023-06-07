ba8523d99a5a1423777185f60900fa06bb13eedf
SAYKACHI <108486488+saykachi@users.noreply.github.com>
2023-06-07 10:15:27 +0300

    Update game-porting-toolkit-compiler.rb

--- Formula/game-porting-toolkit-compiler.rb
+++ Formula/game-porting-toolkit-compiler.rb
@@ -14,47 +14,45 @@
 # OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 class GamePortingToolkitCompiler < Formula
-  version "0.1"
   desc "Compiler for Apple Game Porting Toolkit"
   homepage "https://developer.apple.com/"
   url "https://media.codeweavers.com/pub/crossover/source/crossover-sources-22.1.1.tar.gz", using: :nounzip
   sha256 "cdfe282ce33788bd4f969c8bfb1d3e2de060eb6c296fa1c3cdf4e4690b8b1831"
-  # license ""
-  
+  version "22.1.1"
+
   depends_on "cmake" => :build
   depends_on "ninja" => :build
-  
+
   keg_only :provided_by_macos
-  
+
   def install
-    # The 22.1.1 tarball contains an empty sources/freetype directory, which confuses Homebrew.
-    # So we extract it ourself. This also lets us restrict extraction to just the clang directory.
-    system "tar", "-xf", "crossover-sources-22.1.1.tar.gz", "--include=sources/clang/*", "--strip-components=2"
-    
+    # Extract the required clang files from the tarball
+    system "tar", "-xf", "crossover-sources-#{version}.tar.gz", "--include=sources/clang/*", "--strip-components=2"
+
+    # Build an x86_64-native clang
     mkdir "clang-build" do
-      # Build an x86_64-native clang.
-      system "cmake", "-G", "Ninja",
-                      "-DCMAKE_VERBOSE_MAKEFILE=#{verbose? ? "On" : "Off"}",
-                      "-DCMAKE_INSTALL_PREFIX=#{prefix}",
-                      "-DCMAKE_MAKE_PROGRAM=ninja",
-                      "-DCMAKE_BUILD_TYPE=Release",
-                      "-DCMAKE_VERBOSE_MAKEFILE=On",
-                      "-DCMAKE_OSX_ARCHITECTURES=x86_64",
-                      "-DLLVM_TARGETS_TO_BUILD=X86",
-                      "-DLLVM_NATIVE_ARCH=X86",
-                      "-DLLVM_HOST_TRIPLE=x86_64-apple-darwin",
-                      "-DLLVM_INSTALL_TOOLCHAIN_ONLY=On",
-                      "-DLLVM_ENABLE_PROJECTS=clang",
-                      buildpath/"llvm"
-                      
-        if verbose?
-          system "ninja", "-v", "install"
-        else
-          system "ninja", "install"
-        end
-      end
-      
-    # Sometimes Wine tries to build with GCC even if it can find clang.
+      args = std_cmake_args + %W[
+        -G Ninja
+        -DCMAKE_BUILD_TYPE=Release
+        -DCMAKE_OSX_ARCHITECTURES=x86_64
+        -DLLVM_TARGETS_TO_BUILD=X86
+        -DLLVM_NATIVE_ARCH=X86
+        -DLLVM_HOST_TRIPLE=x86_64-apple-darwin
+        -DLLVM_INSTALL_TOOLCHAIN_ONLY=On
+        -DLLVM_ENABLE_PROJECTS=clang
+      ]
+      system "cmake", *args, buildpath/"sources/clang"
+      system "ninja", "install"
+    end
+
+    # Create a symlink for compatibility with Wine
     bin.install_symlink "clang" => "gcc"
   end
+
+  test do
+    # Add test code here if applicable
+    system "false"
+  end
 end
+
+
